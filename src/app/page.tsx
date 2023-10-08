1b4952df6a90af00c6998d0515bf5d975a55f157
Nhung Ha <nhunghatuyet111202@gmail.com>
2023-10-08 20:44:29 +0700

    change url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 
 export default async function Home() {
-  const res = await fetch("http://localhost:3001/test");
+  const res = await fetch("http://3.26.131.243/test");
   const data = await res.json();
   return (
     <main>
